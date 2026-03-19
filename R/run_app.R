1c02ed0caab147aadb19c90e97e23497d2d66286
Ivan Plyushchenko, PhD <plyush1993@bk.ru>
2026-03-19 22:47:16 +0200

    Add files via upload

--- R/run_app.R
+++ R/run_app.R
@@ -1,9 +1,26 @@
 #' Run the Metabocano Application
-#' Online version: https://plyush1993.shinyapps.io/Metabocano/
 #'
+#'#' @importFrom green bold blue
 #' @export
 run_metabocano <- function(...) {
-  options(shiny.maxRequestSize = 2 * 1024^3)
+  cat("\n")
+  cat(crayon::green("             +--------------------+\n"))
+  app_name <- paste0(
+    crayon::green(crayon::bold("    Metabocano    "))
+  )
+  cat(crayon::green("             | "), app_name, crayon::green(" |\n"), sep = "")
+  cat(crayon::green("             +--------------------+\n"))
+  cat("\n")
+  cat(crayon::blue(crayon::bold("Enhanced interactive volcano plot for metabolomics studies\n")))
+  cat("\n")
+
+  flush.console()
+
+  old_opts <- options(shiny.maxRequestSize = 2 * 1024^3)
+    on.exit({
+    options(old_opts)
+    gc()
+    }, add = TRUE)
   shiny::addResourcePath("www", system.file("www", package = "metabocano"))
   shiny::shinyApp(ui = app_ui(), server = app_server, ...)
 }
