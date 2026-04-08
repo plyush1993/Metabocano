9aa22b0ac614fcf39af4c4304ffcdfb97ae65a72
Ivan Plyushchenko, PhD <plyush1993@bk.ru>
2026-04-08 03:52:27 +0300

    Add files via upload

--- app.R
+++ app.R
@@ -493,15 +493,14 @@ tags$head(tags$style(HTML("
       sidebarLayout(
         sidebarPanel(
           h3(class = "highlight", "Upload"),
-          fileInput("file_data", "Upload feature table (.csv)", accept = ".csv"),
-          helpText(HTML("<i class='fa fa-info-circle'></i> Need data to test? Download example datasets from our <a href='https://github.com/plyush1993/Metabocano' target='_blank'>GitHub</a>.")),
           selectInput(
             "software_tool",
             "Software tool:",
             choices = c("mzMine" = "mzmine", "xcms" = "xcms", "MS-DIAL" = "msdial", "Default" = "default"),
             selected = "mzmine"
           ),
-
+          fileInput("file_data", "Upload feature table (.csv)", accept = ".csv"),
+          helpText(HTML("<i class='fa fa-info-circle'></i> Need data to test? Download example datasets from our <a href='https://github.com/plyush1993/Metabocano' target='_blank'>GitHub</a>.")),
           tags$hr(),
           uiOutput("col_pickers"),
 
